08c462a6d40b99a9539bf412b2d6d1c5a0c35cc5
lhj <lihj@spreadst.com>
2017-03-10 10:04:51 +0800

    add some type typedef,for example "uint32_t"

--- include/types.h
+++ include/types.h
@@ -14,6 +14,16 @@ typedef unsigned short  ushort;
 typedef unsigned int    uint;
 typedef unsigned long   ulong;
 
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+typedef unsigned long uint64_t;
+
+typedef char int8_t;
+typedef short int16_t;
+typedef int int32_t;
+typedef long int64_t;
+
 /*
 struct dataArray{
 	unsigned int nr;
